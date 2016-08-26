38983a17259e82f20d8823df6ba7da83c81865b5
BazingaLyn <lgl050712@163.com>
2016-08-26 17:47:00 +0800

    注释代码

--- laopopo-remoting/src/main/java/org/laopopo/remoting/model/RemotingTransporter.java
+++ laopopo-remoting/src/main/java/org/laopopo/remoting/model/RemotingTransporter.java
@@ -16,19 +16,45 @@ public class RemotingTransporter extends ByteHolder {
 
 	private static final AtomicLong requestId = new AtomicLong(0l);
 
+	/**
+	 * 请求的类型
+	 * 例如该请求是用来订阅服务的，该请求是用来发布服务的等等的
+	 * 假设 code == 1 代表是消费者订阅服务，则接收方注册中心接到该对象的时候，就会先获取该code，判断如果该code==1 则走订阅服务的处理分支代码
+	 * 假设 code == 2 代表是提供者发布服务，则接收发注册中心接收该对象的时候，也会先获取该code，判断如果该code==2则走发布服务的处理分支代码
+	 */
 	private byte code;
 
+	/**
+	 * 请求的主体信息 {@link CommonCustomBody}是一个接口
+	 * 假如code==1 则CommonCustomBody中则是一些订阅服务的具体信息
+	 * 假如code==2 则CommonCustomBody中则是一些发布服务的具体信息
+	 */
 	private transient CommonCustomBody customHeader;
 
+	/**
+	 * 请求的时间戳
+	 */
 	private transient long timestamp;
 
+	/**
+	 * 请求的id
+	 */
 	private long opaque = requestId.getAndIncrement();
 	
+	/**
+	 * 定义该传输对象是请求还是响应信息
+	 */
 	private byte transporterType;
 
 	protected RemotingTransporter() {
 	}
 	
+	/**
+	 * 创建一个请求传输对象
+	 * @param code 请求的类型
+	 * @param commonCustomHeader 请求的正文
+	 * @return
+	 */
 	public static RemotingTransporter createRequestTransporter(byte code,CommonCustomBody commonCustomHeader){
 		RemotingTransporter remotingTransporter = new RemotingTransporter();
 		remotingTransporter.setCode(code);
@@ -37,7 +63,13 @@ public class RemotingTransporter extends ByteHolder {
 		return remotingTransporter;
 	}
 	
-	
+	/**
+	 * 创建一个响应对象
+	 * @param code 响应对象的类型 
+	 * @param commonCustomHeader 响应对象的正文
+	 * @param opaque 此响应对象对应的请求对象的id
+	 * @return
+	 */
 	public static RemotingTransporter createResponseTransporter(byte code,CommonCustomBody commonCustomHeader,long opaque){
 		RemotingTransporter remotingTransporter = new RemotingTransporter();
 		remotingTransporter.setCode(code);
--- laopopo-remoting/src/main/java/org/laopopo/remoting/netty/RemotingClient.java
+++ laopopo-remoting/src/main/java/org/laopopo/remoting/netty/RemotingClient.java
@@ -33,7 +33,7 @@ public interface RemotingClient extends BaseRemotingService {
 
 	/**
 	 * 注入处理器，例如某个Netty的Client实例，这个实例是Consumer端的，它需要处理订阅返回的信息
-	 * 加入订阅的requestCode 是100，那么给定requestCode特定的处理器processorA,且指定该处理器的线程执行器是executorA
+	 * 假如订阅的requestCode 是100，那么给定requestCode特定的处理器processorA,且指定该处理器的线程执行器是executorA
 	 * 这样做的好处就是业务逻辑很清晰，什么样的业务请求对应特定的处理器
 	 * 一般场景下，不是高并发的场景下，executor是可以复用的，这样减少线程上下文的切换
 	 * @param requestCode
@@ -43,6 +43,11 @@ public interface RemotingClient extends BaseRemotingService {
 	void registerProcessor(final byte requestCode, final NettyRequestProcessor processor, final ExecutorService executor);
 	
 	
+	/**
+	 * 注册channel inactive的处理器
+	 * @param processor
+	 * @param executor
+	 */
 	void registerChannelInactiveProcessor(NettyChannelInactiveProcessor processor, ExecutorService executor);
 
 	/**
@@ -53,6 +58,10 @@ public interface RemotingClient extends BaseRemotingService {
 	boolean isChannelWriteable(final String addr);
 	
 	
+	/**
+	 * 当与server的channel inactive的时候，是否主动重连netty的server端
+	 * @param isReconnect
+	 */
 	void setreconnect(boolean isReconnect);
 
 }
